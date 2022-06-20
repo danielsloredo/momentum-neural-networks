265018358585573d6c33685b3bb5a021f58e35b8
danielsloredo <daniel.sloredo@gmail.com>
2022-06-19 21:53:49 -0400

    Fully connected layer with bias

---
+++ neural_network/ann_fully_connected_layer.py
@@ -0,0 +1,22 @@
+from ann_base_class import Layer
+import numpy as np
+
+class FullyConnectedLayer(Layer): 
+    
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.rand(input_size, output_size) - .5
+        self.bias = np.random.rand(1, output_size)  - .5
+        
+    def forward_propagation(self, input_matrix): 
+        self.input = input_matrix
+        self.output = np.dot(self.input, self.weights) + self.bias
+        return self.output
+
+    def backward_propagation(self, output_derivative, learning_rate):
+        input_derivative = np.dot(output_derivative, self.weights.T)
+        weights_derivative = np.dot(self.input.T, output_derivative)
+
+        self.weights = self.weights - learning_rate * weights_derivative
+        self.bias = self.bias - learning_rate * output_derivative
+        
+        return input_derivative
