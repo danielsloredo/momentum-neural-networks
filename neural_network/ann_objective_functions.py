2f4200eb7f699fb7a50bd7b64a68d7794d76744b
danielsloredo <daniel.sloredo@gmail.com>
2022-06-20 12:08:45 -0400

    activation functions cross entropy

---
+++ neural_network/ann_objective_functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def mean_squared_error(y_obs, y_pred):
+    return np.mean(np.power(y_obs-y_pred, 2))
+
+def mean_squared_error_grad(y_obs, y_pred):
+    return 2 * (y_obs - y_pred)/y_obs.size
+
+def cross_entropy(y_obs, y_pred):
+    loss=-np.sum(y_obs*np.log(y_pred))
+    return loss/float(y_pred.shape[0])
+
+def delta_cross_entropy(y_obs, y_pred):
+    m = y_obs.shape[0]
+    grad = y_pred - y_obs
+    grad = grad/m
+    return grad
